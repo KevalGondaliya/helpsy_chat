31d06ad69a4db03995bdc18f94d4e5f443e2439f
keval gondaliya <keval.gondaliya.km@gmail.com>
2020-12-17 20:22:59 +0530

    chat design added

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
 import logo from './logo.svg'
 import './App.css'
+import usrImg from './assets/avatar.png'
 
 function App() {
 	return (
 		<div className="App">
-			<div class="container">
-				<h3 class="text-center">Sanbot</h3>
-				<div class="messaging">
-					<div class="inbox_msg">
-						<div class="mesgs">
-							<div class="msg_history">
-								<div class="incoming_msg">
-									<div class="received_msg">
-										<div class="received_withd_msg">
+			<div className="container">
+				<div className="UImg">
+					<div className="avatarImg">
+						<img src={usrImg} alt="user image" width="45px" />
+					</div>
+				</div>
+
+				<h3 className="userName">Sanbot</h3>
+				<div className="messaging">
+					<div className="inbox_msg">
+						<div className="mesgs">
+							<div className="lableDayWise">
+								<p>Yesterday</p>
+							</div>
+							<div className="msg_history">
+								<div className="incoming_msg">
+									<div className="received_msg">
+										<div className="received_withd_msg">
 											<p>Test which is a new approach to have all solutions</p>
-											<span class="time_date"> 11:01 AM | June 9</span>
+											<span className="time_date"> 11:01 AM | June 9</span>
 										</div>
 									</div>
 								</div>
-								<div class="outgoing_msg">
-									<div class="sent_msg">
+								<div className="outgoing_msg">
+									<div className="sent_msg">
 										<p>Test which is a new approach to have all solutions</p>
-										<span class="time_date"> 11:01 AM | June 9</span>{' '}
+										<span className="time_date"> 11:01 AM | June 9</span>{' '}
 									</div>
 								</div>
-								<div class="incoming_msg">
-									<div class="received_msg">
-										<div class="received_withd_msg">
+								<div className="incoming_msg">
+									<div className="received_msg">
+										<div className="received_withd_msg">
 											<p>Test, which is a new approach to have</p>
-											<span class="time_date"> 11:01 AM | Yesterday</span>
+											<span className="time_date"> 11:01 AM | Yesterday</span>
 										</div>
 									</div>
 								</div>
-								<div class="outgoing_msg">
-									<div class="sent_msg">
+								<div className="outgoing_msg">
+									<div className="sent_msg">
 										<p>Apollo University, Delhi, India Test</p>
-										<span class="time_date"> 11:01 AM | Today</span>{' '}
+										<span className="time_date"> 11:01 AM | Today</span>{' '}
 									</div>
 								</div>
-								<div class="incoming_msg">
-									<div class="received_msg">
-										<div class="received_withd_msg">
+								<div className="incoming_msg">
+									<div className="received_msg">
+										<div className="received_withd_msg">
 											<p>
-												We work directly with our designers and suppliers, and sell direct to you,
-												which means quality, exclusive products, at a price anyone can afford.
+												We work directly with our designers and suppliers, <br />
+												<a href="https://kevalgondaliya.github.io/">https://www.upwork.com/</a>
 											</p>
-											<span class="time_date"> 11:01 AM | Today</span>
+											<span className="time_date"> 11:01 AM | Today</span>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="type_msg">
-								<div class="input_msg_write">
-									<input type="text" class="write_msg" placeholder="Type a message" />
-									<button class="msg_send_btn" type="button">
-										<i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+							<div className="type_msg">
+								<div className="input_msg_write">
+									<input type="text" className="write_msg" placeholder="Type a message" />
+									<button className="msg_send_btn" type="button">
+										<i className="fa fa-paper-plane-o" aria-hidden="true"></i>
 									</button>
 								</div>
 							</div>
