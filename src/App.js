fe5ec89d34e77644afb331056266575a1defd4ea
keval gondaliya <keval.gondaliya.km@gmail.com>
2020-12-18 14:59:34 +0530

    long press like and dislike design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg'
+import React from 'react';
 import './App.css'
 import usrImg from './assets/avatar.png'
 import sendIcon from './assets/send.png'
+import TextComponent from './../src/components/TextComponent'
 
-function App() {
+const App = () => {
 	return (
 		<div className="App">
 			<div className="headerStyle">
@@ -15,141 +17,13 @@ function App() {
 				<h3 className="userName">Sanbot</h3>
 			</div>
 			<div className="container">
-				<div className="messaging">
-					<div className="inbox_msg">
-						<div className="mesgs">
-							<div className="msg_history">
-								<div className="lableDayWise">
-									<p>Yesterday</p>
-								</div>
-								<div className="incoming_msg">
-									<div className="received_msg">
-										<div className="received_withd_msg">
-											<p>Hi, I'm San</p>
-										</div>
-									</div>
-								</div>
-								<div className="incoming_msg">
-									<div className="received_msg">
-										<div className="received_withd_msg">
-											<p>How can i Help you today?</p>
-											<span className="time_date"> 11:01 AM</span>
-										</div>
-									</div>
-								</div>
-
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>I want to learn more about common cancer symptoms</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-
-								<div className="incoming_msg">
-									<div className="received_msg">
-										<div className="received_withd_msg">
-											<p>Which cancer do you want to know more about?</p>
-											<span className="time_date"> 11:01 AM</span>
-										</div>
-									</div>
-								</div>
-								<div className="lableDayWise">
-									<p>Today</p>
-								</div>
-								<div className="outgoing_msg">
-									<div className="sent_msg">
-										<p>Thyroid cancer</p>
-										<span className="time_date_out"> 11:01 AM</span>
-									</div>
-								</div>
-								<div className="incoming_msg">
-									<div className="received_msg">
-										<div className="received_withd_msg">
-											<p>
-												Here is an article about common thyroid cancer symptoms:
-												<br />
-												<a href="https://www.thyroid-cancer.com">https://www.thyroid-cancer.com</a>
-											</p>
-											<span className="time_date"> 11:01 AM</span>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				<TextComponent />
 			</div>
 			<div className="type_msg">
 				<div className="input_msg_write">
 					<input type="text" className="write_msg" placeholder="Type a message here..." />
 					<button className="msg_send_btn" type="button">
-						<img src={sendIcon} alt="send"  />
+						<img src={sendIcon} alt="send" />
 					</button>
 				</div>
 			</div>
---
+++ src/components/TextComponent.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import './../App.css'
+import useLongPress from './useLongPress'
+import like from './../assets/like.png'
+import dislike from './../assets/dislike.png'
+
+const TextComponent = props => {
+	const [longPress, setLongPress] = useState(false)
+	const onLongPress = () => {
+		setLongPress(true)
+	}
+
+	const onClick = () => {
+		console.log('click is triggered')
+	}
+
+	const onLike = () => {
+		setLongPress(false)
+	}
+	const defaultOptions = {
+		shouldPreventDefault: true,
+		delay: 500,
+	}
+	const longPressEvent = useLongPress(onLongPress, onClick, defaultOptions)
+
+	return (
+		<div className="messaging">
+			<div className="inbox_msg">
+				<div className="mesgs">
+					<div className="msg_history">
+						<div className="lableDayWise">
+							<p>Yesterday</p>
+						</div>
+						<div className="incoming_msg">
+							<div className="received_msg">
+								<div className="received_withd_msg" {...longPressEvent}>
+									<p>Hi, I'm San</p>
+								</div>
+								{longPress && (
+									<div className="likesBtn" onClick={onLike}>
+										<img src={like} alt="user image" width="20px" id="likeImg" />
+										<img src={dislike} alt="user image" width="20px" />
+									</div>
+								)}
+							</div>
+						</div>
+						<div className="incoming_msg">
+							<div className="received_msg">
+								<div className="received_withd_msg">
+									<p>How can i Help you today?</p>
+									<span className="time_date"> 11:01 AM</span>
+								</div>
+							</div>
+						</div>
+						<div className="outgoing_msg">
+							<div className="sent_msg">
+								<p>I want to learn more about common cancer symptoms</p>
+								<span className="time_date_out"> 11:01 AM</span>
+							</div>
+						</div>
+						<div className="incoming_msg">
+							<div className="received_msg">
+								<div className="received_withd_msg">
+									<p>Which cancer do you want to know more about?</p>
+									<span className="time_date"> 11:01 AM</span>
+								</div>
+							</div>
+						</div>
+						<div className="lableDayWise">
+							<p>Today</p>
+						</div>
+						<div className="outgoing_msg">
+							<div className="sent_msg">
+								<p>Thyroid cancer</p>
+								<span className="time_date_out"> 11:01 AM</span>
+							</div>
+						</div>
+						<div className="incoming_msg">
+							<div className="received_msg">
+								<div className="received_withd_msg">
+									<p>
+										Here is an article about common thyroid cancer symptoms:
+										<br />
+										<a href="https://www.thyroid-cancer.com">https://www.thyroid-cancer.com</a>
+									</p>
+									<span className="time_date"> 11:01 AM</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default TextComponent
